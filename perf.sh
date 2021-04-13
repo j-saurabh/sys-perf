c6606b5d7b83b759a72241a8318c331393f9250f
SAURABH JOSHI <64773452+j-saurabh@users.noreply.github.com>
2021-04-13 13:49:41 +0530

    Added comments

--- perf.sh
+++ perf.sh
@@ -1,13 +1,19 @@
 #! /bin/bash
 
+#Used date and time so it shows when the time when script runs and the memory details for that time.
+
 date=`date '+%B %d %Y'`
 time1=`date '+%r'`
 
-total_mem=`free -h | head -2 | tail -1 |  awk '{print $2}'`
+#For total, used and free memory, 'free' command shows more details. Here we have scrapped other values only to display the required values.
+
+total_mem=`free -h | head -2 | tail -1 |  awk '{print $2}'` 
 
 used_mem=`free -h | head -2 | tail -1 |  awk '{print $3}'`
 
 free_mem=`free -h | head -2 | tail -1 |  awk '{print $4}'`
+
+#For disk space, df command is used here. For specific directory you can use du also. 
 
 total_disk=`df -h --output=size --total | awk 'END {print $1}'`
 
@@ -23,11 +29,11 @@ echo -e "Time: $time1\n"
 
 echo "------------------------------------------------"
 
+#Used the tabs in order to display values in my editor.
 echo -e "\n\t\tTotal\tUsed\tAvailable \n"
 
 echo -e "RAM : \t\t$total_mem\t$used_mem\t$free_mem" 
 
 echo -e "\nDisk Space: \t$total_disk\t$used_disk\t$avail_disk\n"
-
 
 
